8d016a39eb7903c08602262bfcb212c0cab5c774
f3nrisw0lf <rodrickcorpuz@gmail.com>
2021-05-07 14:26:16 +0800

    Added CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cors = require('cors');
 const LocalStrategy = require('passport-local').Strategy;
 
 const signup = require('./api/signup.js');
@@ -33,6 +34,7 @@ app.use(
 	}),
 );
 app.use(express.json());
+app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
