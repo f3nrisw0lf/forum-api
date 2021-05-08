159e9466b10112d71877c0e0450bdfc94a53ad72
f3nrisw0lf <rodrickcorpuz@gmail.com>
2021-05-08 19:20:49 +0800

    Added Login Route in Getting User by UserID

--- api/login.js
+++ api/login.js
@@ -7,4 +7,14 @@ router.post('/', passport.authenticate('local'), (req, res) => {
 	res.json(req.body);
 });
 
+router.get('/:userId', async (req, res) => {
+	const { userId } = req.params;
+	try {
+		const user = await User.findOne({ _id: userId });
+		res.json(user);
+	} catch (error) {
+		res.json(error);
+	}
+});
+
 module.exports = router;
